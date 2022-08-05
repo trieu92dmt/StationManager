48f46e89693e553e4855bb90a3322751bfe719e3
Long Phan Tiến <long.pt@isdcorp.vn>
2022-08-05 15:21:59 +0700

    Weight API

---
+++ API.Weight/Program.cs
@@ -0,0 +1,41 @@
+using EntityModels.Datas;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Weight", Version = "v1" });
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ EntityModels/EntityModels/ResourceMonitorModel.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityModels.EntityModels
+{
+    public class ResourceMonitorModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string WeightDetailId { get; set; }
+        [StringLength(50)]
+        public string WeighingStationCode { get; set; }
+        public DateTime CheckingTime { get; set; }
+        public double WeightNumberDisplayed { get; set; }
+    }
+}

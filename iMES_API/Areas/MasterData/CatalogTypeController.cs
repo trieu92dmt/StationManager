01f67d8ffa784c040ffb594903667d44fc21476e
kienvtisd <kien.vt@isdcorp.vn>
2022-12-29 16:38:42 +0700

    Create CatalogType

---
+++ iMES_API/Areas/MasterData/CatalogTypeController.cs
@@ -0,0 +1,38 @@
+﻿using ISD.API.Applications.Commands.Catalog;
+using ISD.API.Applications.Commands.Company;
+using ISD.API.Applications.DTOs.Company;
+using ISD.API.Core.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+
+namespace TLG_API.Areas.MasterData
+{
+    [Route("api/v{version:apiVersion}/MasterData/[controller]")]
+    [ApiVersion("1.0")]
+    [ApiController]
+    public class CatalogTypeController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CatalogTypeController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("create-catalog-type")]
+        public async Task<IActionResult> CreateCatalogType(CatalogTypeCreateCommand req)
+        {
+            var response = await _mediator.Send(req);
+
+            return Ok(new ApiSuccessResponse<bool>()
+            {
+                IsSuccess= true,
+                Data = response
+            });
+        }
+    }
+}

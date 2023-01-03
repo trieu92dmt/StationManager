eaebbe5e41c60d1da4b2555d6e7bfa8d18902116
trieu92isd <trieu.dm@isdcorp.vn>
2023-01-03 11:18:36 +0700

    plant dropdown api

---
+++ ISD.API.Applications/Queries/MasterData/CatalogQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISD.API.Applications.Queries.MasterData
+{
+    public interface ICatalogQuery
+    {
+
+    }
+
+    public class CatalogQuery
+    {
+
+    }
+}

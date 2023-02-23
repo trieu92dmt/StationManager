761ef7262f89c4fe069eec689aa7740b36ea865c
trieu92isd <trieu.dm@isdcorp.vn>
2023-02-23 16:21:29 +0700

    bảng 2 nhlt

--- src/Services/MES/MES.Application/Queries/NHLTQuery.cs
+++ src/Services/MES/MES.Application/Queries/NHLTQuery.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
@@ -28,6 +29,13 @@ namespace MES.Application.Queries
         /// <param name="command"></param>
         /// <returns></returns>
         Task<List<GetInputDataResponse>> GetInputDatas(SearchNHLTCommand command);
+
+        /// <summary>
+        /// Lấy dữ liệu đã lưu nhlt
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        Task<List<SearchNHLTResponse>> GetDataNHLT(SearchNHLTCommand command);  
     }
 
     public class NHLTQuery : INHLTQuery
@@ -48,6 +56,119 @@ namespace MES.Application.Queries
             _dtOdRepo = dtOdRepo;
             _cusRepo = cusRepo;
             _slocRepo = slocRepo;
+        }
+
+        public Task<List<SearchNHLTResponse>> GetDataNHLT(SearchNHLTCommand command)
+        {
+            #region Format Day
+
+            if (command.DocumentDateFrom.HasValue)
+            {
+                command.DocumentDateFrom = command.DocumentDateFrom.Value.Date;
+            }
+            if (command.DocumentDateTo.HasValue)
+            {
+                command.DocumentDateTo = command.DocumentDateTo.Value.Date.AddDays(1).AddSeconds(-1);
+            }
+            //Ngày cân
+            if (command.WeightDateFrom.HasValue)
+            {
+                command.WeightDateFrom = command.WeightDateFrom.Value.Date;
+            }
+            if (command.WeightDateTo.HasValue)
+            {
+                command.WeightDateTo = command.WeightDateTo.Value.Date.AddDays(1).AddSeconds(-1);
+            }
+            #endregion
+
+            //Tạo query
+            var query = _nhltRepo.GetQuery()
+                                 .Include(x => x.DetailOD).ThenInclude(x => x.DetailOutboundDeliveryId)
+                                 .AsNoTracking();
+
+            //Get data theo plant
+            if (!string.IsNullOrEmpty(command.Plant))
+            {
+                query = query.Where(x => x.PlantCode == command.Plant);
+            }
+
+            //Get data theo material
+            if (!string.IsNullOrEmpty(command.MaterialFrom))
+            {
+                //Nếu ko có to thì search 1
+                if (string.IsNullOrEmpty(command.MaterialTo))
+                {
+                    command.MaterialTo = command.MaterialFrom;
+                }
+
+                query = query.Where(x => x.MaterialCodeInt >= long.Parse(command.MaterialFrom) &&
+                                         x.MaterialCodeInt <= long.Parse(command.MaterialTo));
+            }
+
+            //Get data theo customer
+            if (!string.IsNullOrEmpty(command.CustomerFrom))
+            {
+                //Nếu ko có to thì search 1
+                if (string.IsNullOrEmpty(command.CustomerTo))
+                {
+                    command.CustomerTo = command.CustomerFrom;
+                }
+
+                query = query.Where(x => x.Customer.CompareTo(command.CustomerFrom) >= 0 &&
+                                                 x.Customer.CompareTo(command.CustomerTo) <= 0);
+            }
+
+            //Get data theo Outbound delivery
+            if (!string.IsNullOrEmpty(command.OutboundDeliveryFrom))
+            {
+                //Nếu ko có to thì search 1
+                if (string.IsNullOrEmpty(command.OutboundDeliveryTo))
+                {
+                    command.OutboundDeliveryTo = command.OutboundDeliveryFrom;
+                }
+
+                query = query.Where(x => x.DetailOD.OutboundDelivery.DeliveryCode.CompareTo(command.OutboundDeliveryFrom) >= 0 &&
+                                               x.DetailOD.OutboundDelivery.DeliveryCode.CompareTo(command.OutboundDeliveryTo) <= 0);
+            }
+
+            //Get data theo document date
+            if (command.DocumentDateFrom.HasValue)
+            {
+                if (!command.DocumentDateTo.HasValue)
+                {
+                    command.DocumentDateTo = command.DocumentDateFrom.Value.Date.AddDays(1).AddSeconds(-1);
+                }
+                query = query.Where(x => x.DetailOD.OutboundDelivery.DocumentDate >= command.DocumentDateFrom &&
+                                                 x.DetailOD.OutboundDelivery.DocumentDate <= command.DocumentDateTo);
+            }
+
+            //Search dữ liệu đã cân
+            if (!string.IsNullOrEmpty(command.WeightHeadCode))
+            {
+                query = query.Where(x => !string.IsNullOrEmpty(x.WeightHeadCode) ? x.WeightHeadCode.Trim().ToLower() == command.WeightHeadCode.Trim().ToLower() : false);
+            }
+
+            //Check Ngày thực hiện cân
+            if (command.WeightDateFrom.HasValue)
+            {
+                if (!command.WeightDateTo.HasValue) command.WeightDateTo = command.WeightDateFrom.Value.Date.AddDays(1).AddSeconds(-1);
+                query = query.Where(x => x.CreateTime >= command.WeightDateFrom &&
+                                         x.CreateTime <= command.WeightDateTo);
+            }
+
+            //Check số phiếu cân
+            if (command.WeightVotes != null && command.WeightVotes.Any())
+            {
+                query = query.Where(x => command.WeightVotes.Contains(x.WeightVote));
+            }
+
+            //Check create by
+            if (command.CreateBy.HasValue)
+            {
+                query = query.Where(x => x.CreateBy == command.CreateBy);
+            }
+
+            throw new NotImplementedException();
         }
 
         public async Task<List<CommonResponse>> GetDropDownWeightVote(string keyword)

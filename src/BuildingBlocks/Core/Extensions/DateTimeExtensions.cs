eb6a1cf6a1338beb2759882c9f7d43b512da6763
trieu92isd <trieu.dm@isdcorp.vn>
2023-03-18 13:08:13 +0700

    update api date time extension

---
+++ src/BuildingBlocks/Core/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Core.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime StartOfWeek(this DateTime dt, DayOfWeek startOfWeek)
+        {
+            int diff = (7 + (dt.DayOfWeek - startOfWeek)) % 7;
+            return dt.AddDays(-1 * diff).Date;
+        }
+
+        public static int ConvertDateTimeToInt(DateTime? dateTime)
+        {
+            int dateKey;
+            try
+            {
+                dateKey = Convert.ToInt32(string.Format("{0:yyyyMMdd}", dateTime));
+                return dateKey;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ src/Services/MES/MES.API/Controllers/Utilities/CommonDateController.cs
@@ -0,0 +1,51 @@
+﻿using Core.Models;
+using MES.Application.DTOs.Common;
+using MES.Application.Queries;
+using MES.Application.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MES.API.Controllers.Utilities
+{
+    [Route("api/v{version:apiVersion}/Utilities/[controller]")]
+    [ApiVersion("1.0")]
+    [ApiController]
+    public class CommonDateController : ControllerBase
+    {
+        private readonly ICommonQuery _commonQuery;
+        private readonly ICommonDateService _commonDateService;
+
+        public CommonDateController(ICommonQuery commonQuery, ICommonDateService commonDateService)
+        {
+            _commonQuery = commonQuery;
+            _commonDateService = commonDateService;
+        }
+
+        [HttpGet("GetDateByCommonDate")]
+        public IActionResult Get(string CommonDate)
+        {
+            DateTime? fromDate;
+            DateTime? toDate;
+
+            _commonDateService.GetDateBy(CommonDate, out fromDate, out toDate);
+
+            return Ok(new
+            {
+                FromDate = string.Format("{0:yyyy-MM-dd}", fromDate),
+                ToDate = string.Format("{0:yyyy-MM-dd}", toDate)
+            });
+        }
+
+        //[HttpGet("get-common-date")]
+        //public async Task<IActionResult> GetCommonDate()
+        //{
+        //    var response = await _commonQuery.GetCommonDate();
+
+        //    return Ok(new ApiSuccessResponse<List<CommonResponse>>()
+        //    {
+        //        Data = response
+        //    });
+
+        //}
+    }
+}

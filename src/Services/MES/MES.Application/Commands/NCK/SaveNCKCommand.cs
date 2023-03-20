7e438949234fdb5c1569a2cb378f2d5fcd3afb06
trieu92isd <trieu.dm@isdcorp.vn>
2023-03-20 17:13:53 +0700

    lưu nck

--- src/Services/MES/MES.Application/Commands/NCK/SaveNCKCommand.cs
+++ src/Services/MES/MES.Application/Commands/NCK/SaveNCKCommand.cs
@@ -261,8 +261,9 @@ namespace MES.Application.Commands.NCK
                     //Material Doc id
                     MaterialDocId = matDoc != null ? matDoc.MaterialDocId : null,
                     //5. Reservation
-                    Reservation = !string.IsNullOrEmpty(item.Reservation) ? reservations.FirstOrDefault(x => x.ReservationCodeInt == long.Parse(item.Reservation)).ReservationCode : null,
-                    ReservationInt = !string.IsNullOrEmpty(item.Reservation) ? long.Parse(item.Reservation) : null,
+                    //Reservation = !string.IsNullOrEmpty(item.Reservation) ? reservations.FirstOrDefault(x => x.ReservationCodeInt == long.Parse(item.Reservation)).ReservationCode : null,
+                    Reservation = matDoc != null ? matDoc.Reservation : null,
+                    ReservationInt = matDoc != null ? long.Parse(matDoc.Reservation) : null,
                     //6. PlantCode
                     PlantCode = item.Plant,
                     PlantName = plants.FirstOrDefault(x => x.PlantCode == item.Plant).PlantName,

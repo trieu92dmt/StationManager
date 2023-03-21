87de595c00cd329ecdc5f70ec834fbd9e2858db7
Rim da Silva <long.pt@isdcorp.vn>
2023-03-21 21:38:45 +0700

    update program auth

--- src/Services/Authentication/Authentication.API/Program.cs
+++ src/Services/Authentication/Authentication.API/Program.cs
@@ -21,7 +21,7 @@ try
     var app = builder.Build();
 
     // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
+    if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
     {
         app.UseSwagger();
         app.UseSwaggerUI();
